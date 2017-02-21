26f2eff02c16cdec0e6784c3371e650c5593038a
njcx <1666276469.com>
2017-02-21 12:59:12 +0800

    添加模板

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class About_me(models.Model):
+    name = models.CharField(max_length=10)
+    content = models.TextField()
+    pic_url = models.URLField()
+    def __str__(self):
+        return self.namema
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def __unicode__(self):
+        return self.name
+
+class Article(models.Model):
+    name = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    content = models.TextField()
+    category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.name
+
+
+
+
+
--- django_blog_nicx/settings.py
+++ django_blog_nicx/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'django_blog_nicx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogx',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
